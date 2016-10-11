46717f5df6f6e4dd36ecf5006c14bdc4801f31f2
long <xiaopohailong@outlook.com>
2016-10-11 11:53:42 +0800

    add nuget push command

--- Build/build.bat
+++ Build/build.bat
@@ -1,6 +1,6 @@
 set MAJ=0
 set MIN=0
-set BLD=2
+set BLD=3
 set REV=0
 
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" buildScript.xml /t:Clean /p:Major=%MAJ% /p:Minor=%MIN% /p:Build=%BLD% /p:Revision=%REV%
@@ -12,5 +12,7 @@ cd ..
 dotnet pack project.json --no-build --output nupkgs
 
 cd Build
+
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" buildScript.xml /t:CleanSymbolsPkg /p:Major=%MAJ% /p:Minor=%MIN% /p:Build=%BLD% /p:Revision=%REV%
 
 pause
---
+++ Build/push.bat
@@ -0,0 +1,1 @@
+nuget push ..\nupkgs\*.nupkg -Source https://www.nuget.org/api/v2/package
